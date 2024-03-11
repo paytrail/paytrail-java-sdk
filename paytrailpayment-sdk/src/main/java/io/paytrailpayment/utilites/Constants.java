b2c9d2c429c6b80d098b574ef0a0df6224092a3e
NerdCloud <devs+paytrail@nerdcloud.co>
2024-03-11 11:32:20 +0700

    fix/use-constants-for-header

--- paytrailpayment-sdk/src/main/java/io/paytrailpayment/utilites/Constants.java
+++ paytrailpayment-sdk/src/main/java/io/paytrailpayment/utilites/Constants.java
@@ -2,33 +2,22 @@ package io.paytrailpayment.utilites;
 
 public class Constants {
     public static final String API_ENDPOINT = "https://services.paytrail.com";
-    public static final String CHECKOUT_ACCOUNT = "checkout-account";
-    public static final String CHECKOUT_ALGORITHM = "checkout-algorithm";
-    public static final String CHECKOUT_METHOD = "checkout-method";
-    public static final String CHECKOUT_NONCE = "checkout-nonce";
-    public static final String CHECKOUT_TIMESTAMP = "checkout-timestamp";
-    public static final String PLATFORM_NAME = "platform-name";
+    public static final String CHECKOUT_ACCOUNT_HEADER = "checkout-account";
+    public static final String CHECKOUT_ALGORITHM_HEADER = "checkout-algorithm";
+    public static final String CHECKOUT_METHOD_HEADER = "checkout-method";
+    public static final String CHECKOUT_NONCE_HEADER = "checkout-nonce";
+    public static final String CHECKOUT_TIMESTAMP_HEADER = "checkout-timestamp";
+    public static final String CHECKOUT_TRANSACTION_ID_HEADER = "checkout-transaction-id";
+    public static final String CHECKOUT_TOKENIZATION_ID_HEADER = "checkout-tokenization-id";
+    public static final String SIGNATURE_HEADER = "signature";
+    public static final String PLATFORM_NAME_HEADER = "platform-name";
+    public static final String CONTENT_TYPE_HEADER = "content-type";
     public static final String CONTENT_TYPE = "application/json; charset=utf-8";
-    public static final String CHECKOUT_TRANSACTION_ID = "checkout-transaction-id";
-    public static final String CHECKOUT_TOKENIZATION_ID = "checkout-tokenization-id";
-    public static final String SIGNATURE = "signature";
-    public static final String CHECKOUT_REDIRECT_SUCCESS_URL = "checkout-redirect-success-url";
-    public static final String CHECKOUT_REDIRECT_CANCEL_URL = "checkout-redirect-cancel-url";
-    public static final String CHECKOUT_CALLBACK_SUCCESS_URL = "checkout-callback-success-url";
-    public static final String CHECKOUT_CALLBACK_CANCEL_URL = "checkout-callback-cancel-url";
-    public static final String LANGUAGE = "language";
-    public static final String MESSAGE_CHECKOUT_ACCOUNT_EMPTY = "checkout-account is empty. ";
-    public static final String MESSAGE_CHECKOUT_ALGORITHM_EMPTY = "checkout-algorithm is empty. ";
-    public static final String MESSAGE_UNSUPPORTED_METHOD = "unsupported method chosen. ";
-    public static final String MESSAGE_CHECKOUT_TIMESTAMP_EMPTY = "checkout-timestamp is empty. ";
-    public static final String MESSAGE_CHECKOUT_REDIRECT_SUCCESS_URL_EMPTY = "checkout-redirect success url is empty. ";
-    public static final String MESSAGE_CHECKOUT_REDIRECT_CANCEL_URL_EMPTY = "checkout-redirect cancel url is empty. ";
-    public static final String MESSAGE_UNSUPPORTED_LANGUAGE = "unsupported language chosen. ";
-
-    public static final String EMAIL_REGEX =
-            "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
-
 
     public static final String POST_METHOD = "POST";
     public static final String GET_METHOD = "GET";
+
+    public static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+
+    public static final String SHA256_ALGORITHM = "sha256";
 }
