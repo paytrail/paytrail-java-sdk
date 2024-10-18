c9c8307d8cfa99638c445586198cd4ab58238432
NerdCloud <devs+paytrail@nerdcloud.co>
2024-10-18 16:36:30 +0000

    CreateMitPaymentAuthorizationHold CreateMitPaymentCommit CreateCitPaymentCharge CreateCitPaymentAuthorizationHold CreateCitPaymentCommit PayAndAddCard createAddCardFormRequest createMitPaymentCharge

--- paytrailpayment-sdk/src/main/java/io/paytrailpayment/IPaytrail.java
+++ paytrailpayment-sdk/src/main/java/io/paytrailpayment/IPaytrail.java
@@ -1,10 +1,7 @@
 package io.paytrailpayment;
 
-import io.paytrailpayment.dto.request.CreatePaymentRequest;
-import io.paytrailpayment.dto.request.CreateRefundRequest;
-import io.paytrailpayment.dto.response.CreatePaymentResponse;
-import io.paytrailpayment.dto.response.CreateRefundResponse;
-import io.paytrailpayment.dto.response.GetPaymentResponse;
+import io.paytrailpayment.dto.request.*;
+import io.paytrailpayment.dto.response.*;
 
 /**
  * Defines methods for interacting with the Paytrail payment system.
@@ -27,6 +24,7 @@ public interface IPaytrail {
      *    Note! The transaction id needs to be sent on checkout-transaction-id header as well.
      *
      * @param transactionId The unique identifier for the transaction.
+     * @return GetPaymentResponse
      * @see <a href="https://docs.paytrail.com/#/?id=get">Paytrail Documentation - Get Payment</a>
      */
     GetPaymentResponse getPaymentInfo(String transactionId);
@@ -36,7 +34,74 @@ public interface IPaytrail {
      *
      * @param refundRequest   The request object containing refund details.
      * @param transactionId   The unique identifier for the transaction to refund.
+     * @return CreateRefundResponse
      * @see <a href="https://docs.paytrail.com/#/?id=refund">Paytrail Documentation - Create Refund Request</a>
      */
     CreateRefundResponse createRefundRequest(CreateRefundRequest refundRequest, String transactionId);
+
+    /**
+     * HTTP POST /tokenization/addcard-form add card form.
+     *
+     * @param req   The request object containing add card request
+     * @return AddCardFormResponse
+     * @see <a href="https://docs.paytrail.com/#/?id=add-card-form">Paytrail Documentation - Create Refund Request</a>
+     */
+    AddCardFormResponse createAddCardFormRequest(AddCardFormRequest req);
+
+    /**
+     * HTTP POST /payments/token/mit/charge creates a new MIT payment charge.
+     * @param req
+     * @return CreateMitPaymentChargeResponse
+     * @see <a href="https://docs.paytrail.com/#/?id=create-authorization-hold-or-charge">Paytrail Documentation  - Create authorization hold or charge
+     */
+    CreateMitOrCitPaymentResponse createMitPaymentCharge(CreateMitOrCitPaymentRequest req);
+    /**
+     * HTTP POST /payments/token/mit/authorization-hold creates a new MIT authorization hold.
+     * @param createMitPaymentAuthorizationHold
+     * @return CreateMitPaymentChargeResponse
+     * @see <a href="https://docs.paytrail.com/#/?id=create-authorization-hold-or-charge">Paytrail Documentation  - Create authorization hold or charge
+     */
+    CreateMitOrCitPaymentResponse createMitPaymentAuthorizationHold(CreateMitOrCitPaymentRequest createMitPaymentAuthorizationHold);
+
+    /**
+     * HTTP POST /payments/{transactionId}/token/commit commits a MIT payment.
+     * @param req
+     * @param transactionId
+     * @return CreateMitOrCitPaymentResponse
+     * @see <a href="https://docs.paytrail.com/#/?id=commit-authorization-hold">Paytrail Documentation  - Create MIT payment commit
+     */
+    CreateMitOrCitPaymentResponse createMitPaymentCommit(CreateMitOrCitPaymentRequest req, String transactionId);
+
+    /**
+     * HTTP POST /payments/token/cit/charge creates a new CIT payment charge.
+     * @param req
+     * @return CreateMitOrCitPaymentResponse
+     * @see <a href="https://docs.paytrail.com/#/?id=create-authorization-hold-or-charge">Paytrail Documentation  - Create authorization hold or charge
+     */
+    CreateMitOrCitPaymentResponse createCitPaymentCharge(CreateMitOrCitPaymentRequest req);
+
+
+    /**
+     * HTTP POST /payments/token/cit/authorization-hold creates a new CIT authorization hold.
+     * @param req
+     * @return CreateMitOrCitPaymentResponse
+     * @see <a href="https://docs.paytrail.com/#/?id=create-authorization-hold-or-charge">Paytrail Documentation  - Create CIT authorization hold
+     */
+    CreateMitOrCitPaymentResponse createCitPaymentAuthorizationHold(CreateMitOrCitPaymentRequest req);
+
+    /**
+     * HTTP POST /payments/{transactionId}/token/commit commits a CIT payment.
+     * @param req
+     * @param transactionId
+     * @return CreateMitOrCitPaymentResponse
+     * @see <a href="https://docs.paytrail.com/#/?id=commit-authorization-hold">Paytrail Documentation  - Create CIT payment commit
+     */
+    CreateMitOrCitPaymentResponse createCitPaymentCommit(CreateMitOrCitPaymentRequest req, String transactionId);
+
+    /**
+     * HTTP POST /tokenization/pay-and-add-card pay and add card.
+     * @param req
+     * @return
+     */
+    PayAddCardResponse payAndAddCard(PayAddCardRequest req);
 }
---
+++ paytrailpayment-sdk/src/test/java/io/paytrailpayment/CreateCitPaymentAuthorizationHoldTests.java
@@ -0,0 +1,139 @@
+package io.paytrailpayment;
+
+import io.paytrailpayment.dto.request.*;
+import io.paytrailpayment.dto.request.model.*;
+import io.paytrailpayment.dto.response.CreateMitOrCitPaymentResponse;
+import io.paytrailpayment.utilites.ResponseMessage;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CreateCitPaymentAuthorizationHoldTests {
+    private static final String MERCHANTIDN = "375917";
+    private static final String MERCHANTIDSIS = "695861";
+    private static final String SECRETKEYSIS = "SAIPPUAKAUPPIAS";
+
+    @Test
+    public void createCitPaymentAuthorizationHold_RequestNull_ReturnCode400() {
+        // Arrange
+        int expected = ResponseMessage.RESPONSE_NULL.getCode();
+
+        // Act
+        PaytrailClient payTrail = new PaytrailClient(MERCHANTIDN, SECRETKEYSIS, "test");
+        CreateMitOrCitPaymentRequest request = null;
+        CreateMitOrCitPaymentResponse res = payTrail.createCitPaymentAuthorizationHold(request);
+        int actual = res.getReturnCode();
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void createCitPaymentAuthorizationHold_ValidateFalse_ReturnCode403() {
+        // Arrange
+        int expected = ResponseMessage.VALIDATION_FAILED.getCode();
+
+        // Act
+        PaytrailClient payTrail = new PaytrailClient(MERCHANTIDN, SECRETKEYSIS, "test");
+        CreateMitOrCitPaymentRequest request = new CreateMitOrCitPaymentRequest();
+        CreateMitOrCitPaymentResponse res = payTrail.createCitPaymentCharge(request);
+        int actual = res.getReturnCode();
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void createCitPaymentAuthorizationHold_Success_ReturnCode200() {
+        // Arrange
+        int expected = ResponseMessage.OK.getCode();
+
+        // Act
+        PaytrailClient payTrail = new PaytrailClient(MERCHANTIDN, SECRETKEYSIS, "test");
+        CreateMitOrCitPaymentRequest payload = createValidPayload();
+        CreateMitOrCitPaymentResponse res = payTrail.createCitPaymentCharge(payload);
+        int actual = res.getReturnCode();
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void createCitPaymentAuthorizationHold_CallPaytrailReturnFail_ReturnCode500() {
+        // Arrange
+        int expected = ResponseMessage.RESPONSE_ERROR.getCode();
+
+        // Act
+        PaytrailClient payTrail = new PaytrailClient(MERCHANTIDSIS, SECRETKEYSIS, "test");
+        CreateMitOrCitPaymentRequest payload = createValidPayload();
+        CreateMitOrCitPaymentResponse res = payTrail.createCitPaymentCharge(payload);
+        int actual = res.getReturnCode();
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    private CreateMitOrCitPaymentRequest createValidPayload() {
+        CreateMitOrCitPaymentRequest payload = new CreateMitOrCitPaymentRequest();
+        payload.setToken("c7441208-c2a1-4a10-8eb6-458bd8eaa64f");
+        payload.setStamp(UUID.randomUUID().toString());
+        payload.setReference("9187445");
+        payload.setAmount(1590);
+        payload.setCurrency(PaytrailCurrency.EUR);
+        payload.setLanguage(PaytrailLanguage.FI);
+        payload.setOrderId("");
+
+        ShopInShopItem item = new ShopInShopItem();
+        item.setUnitPrice(1590);
+        item.setUnits(1);
+        item.setVatPercentage(24);
+        item.setProductCode("#927502759");
+        item.setCategory("Pet supplies");
+        item.setDescription("Cat ladder");
+        item.setStamp(UUID.randomUUID().toString());
+        item.setReference("9187445");
+        payload.setItems(Arrays.asList(item));
+
+        Customer customer = new Customer();
+        customer.setEmail("erja.esimerkki@example.org");
+        customer.setFirstName("Erja");
+        customer.setVatId("FI12345671");
+        customer.setCompanyName("nothing");
+        customer.setLastName("+358501234567");
+        customer.setPhone("123");
+        payload.setCustomer(customer);
+
+        CallbackUrl redirectUrls = new CallbackUrl();
+        redirectUrls.setSuccess("https://ecom.example.org/success");
+        redirectUrls.setCancel("https://ecom.example.org/cancel");
+        payload.setRedirectUrls(redirectUrls);
+
+        CallbackUrl callbackUrls = new CallbackUrl();
+        callbackUrls.setSuccess("https://ecom.example.org/success");
+        callbackUrls.setCancel("https://ecom.example.org/cancel");
+        payload.setCallbackUrls(callbackUrls);
+
+        Address deliveryAddress = new Address();
+        deliveryAddress.setCity("Tampere");
+        deliveryAddress.setCountry("FI");
+        deliveryAddress.setCounty("Pirkanmaa");
+        deliveryAddress.setPostalCode("33100");
+        deliveryAddress.setStreetAddress("Hämeenkatu 6 B");
+        payload.setDeliveryAddress(deliveryAddress);
+
+        Address invoicingAddress = new Address();
+        invoicingAddress.setCity("Helsinki");
+        invoicingAddress.setCountry("FI");
+        invoicingAddress.setCounty("Uusimaa");
+        invoicingAddress.setPostalCode("00510");
+        invoicingAddress.setStreetAddress("Testikatu 1");
+        payload.setInvoicingAddress(invoicingAddress);
+
+        payload.setGroups(Arrays.asList(PaytrailPaymentMethodGroup.MOBILE.toString()));
+
+        return payload;
+    }
+}
